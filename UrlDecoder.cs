c8c09476398c2bc874020adffb723fc2f9d2272c
LVVeselinov <lyudmil_v_veselinov@abv.bg>
2018-09-21 13:35:02 +0300

    homework

---
+++ UrlDecoder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+
+namespace UrlDecode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Url = Console.ReadLine();
+
+            while (Url != string.Empty)
+            {
+                Console.WriteLine(WebUtility.UrlDecode(Url));
+                Uri uri;
+                
+                bool result = Uri.TryCreate(WebUtility.UrlDecode(Url), UriKind.Absolute, out uri)
+                    && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps) ;
+                if (result)
+                {
+                    Console.WriteLine($"Protocol:{uri.Scheme}");
+                    Console.WriteLine($"Host:{uri.Host}");
+                    Console.WriteLine($"Port:{uri.Port}");
+                    Console.WriteLine($"Path:{uri.LocalPath}");
+                    if (uri.Query != string.Empty)
+                        Console.WriteLine($"Query:{uri.Query}");
+                    if (uri.Query != string.Empty)
+                        Console.WriteLine($"Fragment:{uri.Fragment}");
+                }
+                else Console.WriteLine("Invalid Url");
+                
+                Url = Console.ReadLine();
+               
+            }
+            
+            
+            
+        }
+
+    }
+}
